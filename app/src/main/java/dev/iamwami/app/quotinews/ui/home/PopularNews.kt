b87e72030b0cccc5c8a5dee940cf1059695ffef2
Wami Ikechukwu <wamiikechukwu@gmail.com>
2022-09-27 22:26:16 +0100

    Removed the card from PopularNews.kt composable

--- app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
@@ -1,7 +1,6 @@
 package dev.iamwami.app.quotinews.ui.home
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -26,31 +26,37 @@ fun PopularNews(
     modifier: Modifier = Modifier,
     newsData: NewsApiResult
 ) {
-    Card(
+    Column(
         modifier = modifier
-            .width(250.dp)
-            .height(300.dp),
+            .requiredWidthIn(min = 250.dp, max = 350.dp)
+            .padding(start = 8.dp, top = 4.dp),
     ) {
-        Column() {
 //            Coil image loader
-            AsyncImage(
-                model = newsData.articles.post.urlToImage,
-                contentDescription = "preview image from the news source",
-                modifier = modifier
-                    .heightIn(min = 180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = MaterialTheme.shapes.medium),
-                placeholder = painterResource(id = R.drawable.news_article_image_placeholder_two),
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = newsData.status,
-                style = MaterialTheme.typography.subtitle1
-            )
+        AsyncImage(
+            model = newsData.articles.post.urlToImage,
+            contentDescription = "preview image from the news source",
+            modifier = modifier
+                .heightIn(min = 180.dp)
+                .fillMaxWidth()
+                .clip(shape = MaterialTheme.shapes.medium),
+            placeholder = painterResource(id = R.drawable.news_article_image_placeholder_two),
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+            text = newsData.articles.post.title,
+            style = MaterialTheme.typography.h6,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2
+        )
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = newsData.articles.post.author,
+            style = MaterialTheme.typography.subtitle2,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
 
-
-        }
     }
 }
 
@@ -72,8 +78,8 @@ class SampleNewsApiDataProvider : PreviewParameterProvider<NewsApiResult> {
                 ),
                 post = Post(
                     urlToImage = "https://appwrite.io/images-ee/1.0/Cover.png",
-                    title = "Culinary Inspirations",
-                    author = "Inspirations",
+                    title = "Buhari set to go for 3rd term as president\"",
+                    author = "Wami Ikechukwu",
                     description = "Culinary",
                     url = "https://appwrite.io/images-ee/1.0/Cover.png",
                     publishedAt = "",

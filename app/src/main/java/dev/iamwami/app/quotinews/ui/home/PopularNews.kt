250852c7952f921724d637880279db74c0fbb91a
Wami Ikechukwu <wamiikechukwu@gmail.com>
2022-10-08 21:26:48 +0100

    Popular news card designed and implemented

--- app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
@@ -1,18 +1,19 @@
 package dev.iamwami.app.quotinews.ui.home
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,8 +36,7 @@ import dev.iamwami.app.quotinews.util.Formatter
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PopularNews(
-    modifier: Modifier = Modifier,
-    newsData: NewsApiResult
+    modifier: Modifier = Modifier, newsData: NewsApiResult
 ) {
     Column(
         modifier = modifier
@@ -64,48 +64,68 @@ fun PopularNews(
         )
 //        News description
         Text(
-            modifier = Modifier.padding(bottom = 8.dp),
             text = newsData.articles.post.description,
             style = MaterialTheme.typography.body2,
             overflow = TextOverflow.Ellipsis,
             maxLines = 2,
             color = Color(0x7C000000)
         )
-        Row() {
-//            For the new publisher name and date published
-            Row(modifier = Modifier.background(Color.Red).height(48.dp)) {
-                Text(
-                    modifier = Modifier.padding(bottom = 8.dp),
-                    text = newsData.articles.source.name,
-                    style = MaterialTheme.typography.body2,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
+        PopularNewsBottomRow(newsData)
+    }
 
-                Text(
-                    modifier = Modifier.padding(bottom = 8.dp),
-                    text = Formatter.dateFormatter(newsData.articles.post.publishedAt),
-                    style = MaterialTheme.typography.body2,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-            }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun PopularNewsBottomRow(newsData: NewsApiResult, iconModifier: Modifier = Modifier.height(20.dp), modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(top = 16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.Bottom,
+    ) {
+//            For the new publisher name and date published
+        Row(
+        ) {
+            Text(
+                text = newsData.articles.source.name, style = MaterialTheme.typography.body2, overflow = TextOverflow.Ellipsis, maxLines = 1
+            )
+            Spacer(modifier = modifier.width(8.dp))
+            Text(
+                text = Formatter.dateFormatter(newsData.articles.post.publishedAt), style = MaterialTheme.typography.body2, overflow = TextOverflow.Ellipsis, maxLines = 1
+            )
+        }
+
+        Spacer(modifier = modifier.width(16.dp))
 
 //            For the like and share and option menu icon
-            Row(modifier = Modifier.background(Color.Blue)) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(Icons.Filled.Favorite, contentDescription = "Heart icon for favourite news")
-                }
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(Icons.Filled.Share, contentDescription = "Share icon to share the news")
-                }
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(Icons.Filled.Menu, contentDescription = "Option menu")
-                }
+        Row(
+        ) {
+            IconButton(
+                onClick = {
+                    Log.d("icon_buttons", "This is bookmarked")
+                }, modifier = iconModifier
+            ) {
+                Icon(Icons.Outlined.FavoriteBorder, contentDescription = "Heart icon for favourite news")
+            }
+            IconButton(
+                onClick = {
+                    Log.d("icon_buttons", "This is shared")
+
+                }, modifier = iconModifier
+            ) {
+                Icon(Icons.Filled.Share, contentDescription = "Share icon to share the news")
+            }
+            IconButton(
+                onClick = {
+                    Log.d("icon_buttons", "checked other options")
+
+                }, modifier = iconModifier
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Option menu")
             }
         }
     }
-
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -113,8 +133,7 @@ fun PopularNews(
 @Composable
 fun PreviewPopularNews(
     @PreviewParameter(
-        SampleNewsApiDataProvider::class,
-        1
+        SampleNewsApiDataProvider::class, 1
     ) data: NewsApiResult
 ) {
     PopularNews(newsData = data)
@@ -123,14 +142,10 @@ fun PreviewPopularNews(
 class SampleNewsApiDataProvider : PreviewParameterProvider<NewsApiResult> {
     override val values: Sequence<NewsApiResult> = sequenceOf(
         NewsApiResult(
-            status = "ok:200",
-            totalResults = "",
-            articles = Articles(
+            status = "ok:200", totalResults = "", articles = Articles(
                 source = Source(
-                    id = "",
-                    name = ""
-                ),
-                post = Post(
+                    id = "", name = ""
+                ), post = Post(
                     urlToImage = "https://appwrite.io/images-ee/1.0/Cover.png",
                     title = "Buhari set to go for 3rd term as president\"",
                     author = "Wami Ikechukwu",

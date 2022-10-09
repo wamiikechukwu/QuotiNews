0f025f1f76a466f74d8a6722826ebb672ca5852e
Wami Ikechukwu <wamiikechukwu@gmail.com>
2022-10-09 15:54:16 +0100

    Fix: news description text color now responding to dark mode.

--- app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/ui/home/PopularNews.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -68,7 +67,6 @@ fun PopularNews(
             style = MaterialTheme.typography.body2,
             overflow = TextOverflow.Ellipsis,
             maxLines = 2,
-            color = Color(0x7C000000)
         )
         PopularNewsBottomRow(newsData)
     }

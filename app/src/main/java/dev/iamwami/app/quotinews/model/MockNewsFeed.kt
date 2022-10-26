11c080814979b3aa062248915525ace737aa1eaf
Wami Ikechukwu <wamiikechukwu@gmail.com>
2022-10-26 05:28:17 +0100

    refactor: cleaned out redundant codes.

--- app/src/main/java/dev/iamwami/app/quotinews/model/MockNewsFeed.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/model/MockNewsFeed.kt
@@ -4,5 +4,5 @@ val mockNewsFeed: NewsFeed = NewsFeed(
     popularNews = listOf(news1, news2, news3, news4, news5),
     highlightedNews = listOf(),
     recommendedNews = listOf(),
-    normalNews = listOf()
+    normalNews = listOf(news5, news3, news4, news2, news1, news3, news4, news2)
 )
--- app/src/main/java/dev/iamwami/app/quotinews/ui/components/UiUtils.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/ui/components/UiUtils.kt
@@ -1,8 +1,13 @@
 package dev.iamwami.app.quotinews.ui.components
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -11,13 +16,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import dev.iamwami.app.quotinews.R
-import dev.iamwami.app.quotinews.model.NewsApiResult
+import dev.iamwami.app.quotinews.model.News
 
 //TODO find the best way to work around passing modifier as an arguement.
 @Composable
 fun NewsImageSmall(
     modifier: Modifier = Modifier,
-    newsData: NewsApiResult,
+    newsData: News,
     contentDescription: String = "preview image from the news source"
 ) {
     AsyncImage(
@@ -27,5 +32,26 @@ fun NewsImageSmall(
             .clip(shape = MaterialTheme.shapes.medium),
         placeholder = painterResource(id = R.drawable.news_article_image_placeholder_two),
         contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun NewsListLikeBtn(modifier: Modifier = Modifier) {
+    IconButton(
+        onClick = {
+            Log.d("icon_buttons", "This is bookmarked")
+        }
+    ) {
+        Icon(Icons.Outlined.FavoriteBorder, contentDescription = "Heart icon for favourite news")
+    }
+}
+
+/* full width divider with padding*/
+@Composable
+fun PostDivider() {
+    Divider(
+        modifier = Modifier
+            .padding(14.dp),
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.08f)
     )
 }

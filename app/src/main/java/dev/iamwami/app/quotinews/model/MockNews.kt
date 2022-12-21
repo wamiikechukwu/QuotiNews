9584a58b12927408d4813a890930bb58facf80ef
Wami Ikechukwu <wamiikechukwu@gmail.com>
2022-12-21 19:07:40 +0100

    refactor: modified the data classes in News.kt and MockNews.kt to match the api response.

--- app/src/main/java/dev/iamwami/app/quotinews/model/MockNews.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/model/MockNews.kt
@@ -2,13 +2,13 @@ package dev.iamwami.app.quotinews.model
 
 val news1 = News(
     status = "ok:200",
-    totalResults = "",
-    articles = Articles(
-        source = Source(
-            id = "",
-            name = "Bloomberg News"
-        ),
-        post = Post(
+    totalResults = 200,
+    articles = listOf(
+        Articles(
+            source = Source(
+                id = "1",
+                name = "Bloomberg News"
+            ),
             urlToImage = "https://responsive.fxempire.com/width/600/webp-lossy-70.q50/_fxempire_/2022/10/tagreuters.com2022newsml_LYNXMPEI980781.jpg",
             title = "Buhari set to go for 3rd term as president klsjalkjd oja jkladi hkaloidh",
             author = "Wami Ikechukwu",
@@ -19,19 +19,20 @@ val news1 = News(
             url = "https://appwrite.io/images-ee/1.0/Cover.png",
             publishedAt = "2022-09-29T04:28:47Z",
             content = ""
+
         )
     )
 )
 
 val news2 = News(
     status = "ok:200",
-    totalResults = "",
-    articles = Articles(
-        source = Source(
-            id = "",
-            name = "CNN News"
-        ),
-        post = Post(
+    totalResults = 700,
+    articles = listOf(
+        Articles(
+            source = Source(
+                id = "2",
+                name = "CNN News"
+            ),
             urlToImage = "https://cleantechnica.com/files/2018/07/comma-ai-adds-driver-monitoring.jpg",
             title = "Nigeria is one kind of country",
             author = "Vivian",
@@ -43,15 +44,16 @@ val news2 = News(
     )
 )
 
+
 val news3 = News(
     status = "ok:200",
-    totalResults = "",
-    articles = Articles(
-        source = Source(
-            id = "",
-            name = "BBC News"
-        ),
-        post = Post(
+    totalResults = 1000,
+    articles = listOf(
+        Articles(
+            source = Source(
+                id = "3",
+                name = "BBC News"
+            ),
             urlToImage = "https://cdn.decrypt.co/resize/1024/height/512/wp-content/uploads/2022/10/Celsius-shutterstock_2171884823-16x9-1-gID_1.png",
             title = "Flooding happening all around the world and Nigeria is literal flooded",
             author = "Zamboki lelee",
@@ -59,19 +61,20 @@ val news3 = News(
             url = "https://appwrite.io/images-ee/1.0/Cover.png",
             publishedAt = "2022-10-27T04:00:47Z",
             content = ""
+
         )
     )
 )
 
 val news4 = News(
     status = "ok:200",
-    totalResults = "",
-    articles = Articles(
-        source = Source(
-            id = "",
-            name = "Aljeersa News"
-        ),
-        post = Post(
+    totalResults = 850,
+    articles = listOf(
+        Articles(
+            source = Source(
+                id = "4",
+                name = "Aljeersa News"
+            ),
             urlToImage = "https://imageio.forbes.com/specials-images/imageserve/6341b2021d939b4e05d6637c/0x0.jpg?format=jpg&width=1200",
             title = "USA is a hell of a country but well it has a good economy",
             author = "Vivian",
@@ -80,17 +83,19 @@ val news4 = News(
             publishedAt = "2022-10-27T04:00:47Z",
             content = ""
         )
+
     )
 )
 val news5 = News(
     status = "ok:200",
-    totalResults = "",
-    articles = Articles(
-        source = Source(
-            id = "",
-            name = "CNN News"
-        ),
-        post = Post(
+    totalResults = 10,
+    articles = listOf(
+        Articles(
+            source = Source(
+                id = "5",
+                name = "CNN News"
+            ),
+
             urlToImage = "https://cleantechnica.com/files/2018/07/comma-ai-adds-driver-monitoring.jpg",
             title = "Nigeria is one kind of country",
             author = "Vivian",
--- app/src/main/java/dev/iamwami/app/quotinews/model/News.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/model/News.kt
@@ -2,66 +2,23 @@ package dev.iamwami.app.quotinews.model
 
 data class News(
     val status: String,
-    val totalResults: String,
-    val articles: Articles
+    val totalResults: Int,
+    val articles: List<Articles>
 )
 
 data class Articles(
     val source: Source,
-    val post: Post
+    val urlToImage: String,
+    val title: String,
+    val author: String,
+    val description: String,
+    val url: String,
+    val publishedAt: String,
+    val content: String
 )
 
 data class Source(
     val id: String,
     val name: String
-)
-
-data class Post(
-    val id: Int,
-    val urlToImage: String,
-    val title: String,
-    val author: String,
-    val description: String,
-    val url: String,
-    val publishedAt: String,
-    val content: String
-)
-
-//--- TODO use the data class below and later it will be changed
-
-data class TestingNews(
-    val status: String,
-    val totalResults: Int,
-    val articles: List<TestingArticles>
-)
-
-data class TestingArticles(
-//    val source: TestingSource,
-//    val post: Post,
-    val source: TestingSource,
-    val urlToImage: String,
-    val title: String,
-    val author: String,
-    val description: String,
-    val url: String,
-    val publishedAt: String,
-    val content: String
-)
-
-data class TestingSource(
-    val id: String,
-    val name: String
-)
-
-data class TestingPost(
-//    val id: Int,
-    val source: TestingSource,
-    val urlToImage: String,
-    val title: String,
-    val author: String,
-    val description: String,
-    val url: String,
-    val publishedAt: String,
-    val content: String
 )
 

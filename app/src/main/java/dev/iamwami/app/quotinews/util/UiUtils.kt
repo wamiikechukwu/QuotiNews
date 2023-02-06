733c0c15f1d36f96ae6452b23eaa7909e6b2ada8
Wami Ikechukwu <wamiikechukwu@gmail.com>
2023-02-07 00:14:29 +0100

    feature: Added the related news and refactored some composable functions names.

--- app/src/main/java/dev/iamwami/app/quotinews/util/UiUtils.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/util/UiUtils.kt
@@ -1,6 +1,5 @@
 package dev.iamwami.app.quotinews.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
@@ -12,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -19,6 +19,7 @@ import dev.iamwami.app.quotinews.R
 import dev.iamwami.app.quotinews.model.News
 
 //TODO find the best way to work around passing modifier as an arguement.
+
 @Composable
 fun NewsImage(
     modifier: Modifier = Modifier.clip(shape = MaterialTheme.shapes.medium),
@@ -35,10 +36,11 @@ fun NewsImage(
 }
 
 @Composable
-fun NewsListLikeBtn(modifier: Modifier = Modifier) {
+fun LikeBtn(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     IconButton(
         onClick = {
-            Log.d("icon_buttons", "This is bookmarked")
+//            Toast.makeText(context, "Functionality not available", Toast.LENGTH_SHORT).show()
         }
     ) {
         Icon(Icons.Outlined.FavoriteBorder, contentDescription = "Heart icon for favourite news")

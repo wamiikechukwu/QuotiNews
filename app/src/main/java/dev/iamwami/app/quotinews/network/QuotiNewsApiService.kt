d121675a17b4b8a2fd810f07a922f3c6cfddfd77
Wami Ikechukwu <wamiikechukwu@gmail.com>
2023-03-23 17:09:11 +0100

    refactor: I added multiple api calls, each to correspond with the category chip in the home screen.

--- app/src/main/java/dev/iamwami/app/quotinews/network/QuotiNewsApiService.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/network/QuotiNewsApiService.kt
@@ -1,23 +1,32 @@
 package dev.iamwami.app.quotinews.network
 
-import dev.iamwami.app.quotinews.BuildConfig
-import dev.iamwami.app.quotinews.model.Articles
 import dev.iamwami.app.quotinews.model.News
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface QuotiNewsApiService {
 
+    @GET("/v2/everything?q=technology")
+    suspend fun getNewsByCategoryTech(): News
 
 
-    //    https://newsapi.org/v2/everything?q=tesla&apiKey=883fcfd667104a34ac74c1827fb419e4
-//    https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=883fcfd667104a34ac74c1827fb419e4
-//    search query - keyword, date published, source domain name and language
-//    keyword, sortBy
-    @GET("/v2/everything?q=tesla")
-    suspend fun getNewsByCategory(): News
-//    suspend fun getNewsByCategory(@QueryMap filter: Map<String, String>?): News
+    @GET("/v2/everything?q=education")
+    suspend fun getNewsByCategoryEdu(): News
+
+
+
+    @GET("/v2/everything?q=government")
+    suspend fun getNewsByCategoryGovt(): News
+
+
+    @GET("/v2/everything?q=fashion")
+    suspend fun getNewsByCategoryFashion(): News
+
+
+    @GET("/v2/everything?q=health")
+    suspend fun getNewsByCategoryHealth(): News
+
+
+    @GET("/v2/everything?q=economy")
+    suspend fun getNewsByCategoryEcons(): News
 }
 
-//https://newsapi.org/everything?q=tesla&apiKey=883fcfd667104a34ac74c1827fb419e4
-//https://newsapi.org/v2/everything?q=tesla&from=2022-11-16&sortBy=publishedAt&apiKey=883fcfd667104a34ac74c1827fb419e4

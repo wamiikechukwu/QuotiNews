1c347d4bcfed51830de7a8029bb9920153d22867
Wami Ikechukwu <wamiikechukwu@gmail.com>
2023-03-29 19:59:17 +0100

    feature: I designed the NavigationDrawer.kt to look like the figma design.

--- app/src/main/java/dev/iamwami/app/quotinews/ui/drawer/NavigationDrawer.kt
+++ app/src/main/java/dev/iamwami/app/quotinews/ui/drawer/NavigationDrawer.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -34,7 +34,14 @@ fun NavigationDrawer(
     closeDrawer: () -> Unit,
 ) {
 
-    ModalDrawerSheet(modifier = modifier){
+    val shape = MaterialTheme.shapes.small
+    val isCurrentRouteHome = currentRoute == HomeScreen.route
+    val isCurrentRouteBookmark = currentRoute == BookmarkScreen.route
+
+    val routeHomeColor = if (isCurrentRouteHome) MaterialTheme.colorScheme.primary else Color.Black
+    val routeBookmarkColor = if (isCurrentRouteBookmark) MaterialTheme.colorScheme.primary else Color.Black
+
+    ModalDrawerSheet(modifier = modifier) {
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
@@ -43,27 +50,61 @@ fun NavigationDrawer(
                 .padding(vertical = 10.dp)
         ) {
             Text(
-                text = stringResource(id = R.string.app_name), fontFamily = Fonts.syneFontFamily, fontSize = 30.sp, color = MaterialTheme.colorScheme.primary
+                text = stringResource(id = R.string.app_name),
+                fontFamily = Fonts.syneFontFamily,
+                fontSize = 30.sp,
+                color = MaterialTheme.colorScheme.primary,
 
-            )
+                )
         }
 
         Divider(modifier = modifier.fillMaxWidth(), thickness = 3.dp, color = MaterialTheme.colorScheme.primaryContainer)
 
         NavigationDrawerItem(
-            label = { Text(text = "Home")},
-            selected = currentRoute == HomeScreen.route,
-            icon = { Icon(Icons.Outlined.Home, "home") },
+            label = {
+                Text(
+                    text = "Home",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = routeHomeColor
+                )
+            },
+            selected = isCurrentRouteHome,
+            icon = {
+                Image(
+                    modifier = modifier.padding(start = 5.dp),
+                    imageVector = Icons.Outlined.Home,
+                    contentDescription = "navigation drawer icon",
+                    contentScale = ContentScale.Inside,
+                    colorFilter = ColorFilter.tint(routeHomeColor),
+                )
+            },
             onClick = { navigateToHome(); closeDrawer() },
-            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+            modifier = Modifier.padding(12.dp),
+            shape = shape
         )
 
         NavigationDrawerItem(
-            label = { Text(text = "Bookmarks")},
-            selected = currentRoute == BookmarkScreen.route,
-            icon = { Icon(Icons.Outlined.FavoriteBorder, "bookmark") },
+            label = {
+                Text(
+                    text = "Bookmarks",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = routeBookmarkColor
+
+                )
+            },
+            selected = isCurrentRouteBookmark,
+            icon = {
+                Image(
+                    modifier = modifier.padding(start = 5.dp),
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = "navigation drawer icon",
+                    contentScale = ContentScale.Inside,
+                    colorFilter = ColorFilter.tint(routeBookmarkColor),
+                )
+            },
             onClick = { navigateToBookmark(); closeDrawer() },
-            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+            modifier = Modifier.padding(12.dp),
+            shape = shape
         )
     }
 
@@ -92,7 +133,6 @@ fun DrawerItems(
                 action()
             },
         color = navItemBgColor,
-        shape = androidx.compose.material.MaterialTheme.shapes.small
     ) {
 
         Row(
